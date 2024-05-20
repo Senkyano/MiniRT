b7abe13189c4df9665e227a0f7522cc4006a707f
Rithy-Yano Hoy <rithyyano.hoy@gmail.com>
2024-05-20 18:21:34 +0200

    upd

---
+++ includes/minirt.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 17:05:40 by rihoy             #+#    #+#             */
+/*   Updated: 2024/05/20 17:08:22 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <string.h>
+# include "lib_utils.h"
+# include "get_next_line.h"
+
+#endif
